1098e07c6a8a35bb97d60269a9f875f46913e8d3
Yury Misiyevich <vinchesterok@tut.by>
2019-04-30 13:02:51 +0300

    feat:add move tool

--- palette/index.js
+++ palette/index.js
@@ -1,38 +1,39 @@
-window.onload=function(){
+window.onload=function() {
     //initial state
-    let temp ={
-        currentTool:'',
-        currentColor:'rgb(0, 128, 0)',
-        prevColor:'rgb(204, 204, 204)',
+    let temp = {
+        currentTool: '',
+        currentColor: 'rgb(0, 128, 0)',
+        prevColor: 'rgb(204, 204, 204)',
     };
-    localStorage.setItem("myState",JSON.stringify(temp));
+    localStorage.setItem("myState", JSON.stringify(temp));
     let state = JSON.parse(localStorage.getItem("myState"));
 
 
-//SET CURRENT AND PREV COLORS IN HTML
-    document.getElementById('currentColorImg').style.backgroundColor=state.currentColor;
-    document.getElementById('prevColorImg').style.backgroundColor=state.prevColor;
+    //SET CURRENT AND PREV COLORS IN HTML
+    document.getElementById('currentColorImg').style.backgroundColor = state.currentColor;
+    document.getElementById('prevColorImg').style.backgroundColor = state.prevColor;
 
 
     /*_________________________________________________*/
 
-//CHOOSE TOOL BUCKET
+    //CHOOSE TOOL BUCKET
     ifBucket(document.getElementById('bucket'));
-    function ifBucket(elem){
-        elem.onclick=function(){
-            if (state.currentTool==='bucket'){
-                state.currentTool='';
-                localStorage.setItem("myState",JSON.stringify(state));
+
+    function ifBucket(elem) {
+        elem.onclick = function () {
+            if (state.currentTool === 'bucket') {
+                state.currentTool = '';
+                localStorage.setItem("myState", JSON.stringify(state));
                 document.getElementById('bucket').style.backgroundColor = '';
             }
-            else{
+            else {
                 //reset colors of others elements
                 document.getElementById('transform').style.backgroundColor = '';
                 document.getElementById('move').style.backgroundColor = '';
                 document.getElementById('picker').style.backgroundColor = '';
 
-                state.currentTool='bucket';
-                localStorage.setItem("myState",JSON.stringify(state));
+                state.currentTool = 'bucket';
+                localStorage.setItem("myState", JSON.stringify(state));
                 document.getElementById('bucket').style.backgroundColor = 'red';
             }
         }
@@ -40,23 +41,24 @@ window.onload=function(){
 
     /*_________________________________________________*/
 
-//CHOOSE TOOL TRANSFORM
+    //CHOOSE TOOL TRANSFORM
     ifTransform(document.getElementById('transform'));
-    function ifTransform(elem){
-        elem.onclick=function(){
-            if (state.currentTool==='transform'){
-                state.currentTool='';
-                localStorage.setItem("myState",JSON.stringify(state));
+
+    function ifTransform(elem) {
+        elem.onclick = function () {
+            if (state.currentTool === 'transform') {
+                state.currentTool = '';
+                localStorage.setItem("myState", JSON.stringify(state));
                 document.getElementById('transform').style.backgroundColor = '';
             }
-            else{
+            else {
                 //reset colors of others elements
                 document.getElementById('bucket').style.backgroundColor = '';
                 document.getElementById('move').style.backgroundColor = '';
                 document.getElementById('picker').style.backgroundColor = '';
 
-                state.currentTool='transform';
-                localStorage.setItem("myState",JSON.stringify(state));
+                state.currentTool = 'transform';
+                localStorage.setItem("myState", JSON.stringify(state));
                 document.getElementById('transform').style.backgroundColor = 'red';
             }
         }
@@ -65,8 +67,9 @@ window.onload=function(){
 
     /*_________________________________________________*/
 
-//COLOR PICKER CHOOSE TOOL
+    //COLOR PICKER CHOOSE TOOL
     ifPicker(document.getElementById('picker'));
+
     function ifPicker(elem) {
         elem.onclick = function () {
             if (state.currentTool === 'picker') {
@@ -89,172 +92,121 @@ window.onload=function(){
 
 
     /*_________________________________________________*/
-//Canvas-item functional
-    Array.from(document.getElementsByClassName('canvas_item')).forEach( elem => {
-        elem.onclick = function itemClick(){
-            //bucket listener
-            if(state.currentTool==='bucket'){
-                elem.style.backgroundColor=state.currentColor;
-            }
-            //transform listener
-            if(state.currentTool==='transform'){
-                if(elem.style.borderRadius===''){
-                    elem.style.borderRadius='calc((60vh - 20px) / 3)';
-                }
-                else{
-                    elem.style.borderRadius='';
-                }
-            }
-            //color picker listener
-            if(state.currentTool==='picker'){
-                let style=getComputedStyle(elem);
-                //check for duplicate colors
-                if(style.backgroundColor!==state.currentColor){
-                    let temp=state.currentColor;
-                    state.currentColor = style.backgroundColor;
-                    state.prevColor=temp;
-                    localStorage.setItem("myState", JSON.stringify(state));
-                    //SET CURRENT AND PREV COLORS IN HTML
-                    document.getElementById('currentColorImg').style.backgroundColor=state.currentColor;
-                    document.getElementById('prevColorImg').style.backgroundColor=state.prevColor;
-                }
-            }
-        }
-    });
 
-    /*_________________________________________________*/
-//CHOOSE RED AND BLUE COLORS BY PICKER
+    //CHOOSE RED AND BLUE COLORS BY PICKER
     chooseColor(document.querySelector('#Blue'));
     chooseColor(document.querySelector('#Red'));
     chooseColor(document.querySelector('#prevColor'));
+
     function chooseColor(elem) {
-        let style=getComputedStyle(elem.childNodes[0]);
+        let style = getComputedStyle(elem.childNodes[0]);
         elem.onclick = function () {
-            if (state.currentColor !== style.backgroundColor) {
-                let temp=state.currentColor;
+            if (state.currentColor !== style.backgroundColor && state.currentTool === 'picker') {
+                let temp = state.currentColor;
                 state.currentColor = style.backgroundColor;
-                state.prevColor=temp;
+                state.prevColor = temp;
                 localStorage.setItem("myState", JSON.stringify(state));
                 //SET CURRENT AND PREV COLORS IN HTML
-                document.getElementById('currentColorImg').style.backgroundColor=state.currentColor;
-                document.getElementById('prevColorImg').style.backgroundColor=state.prevColor;
-
+                document.getElementById('currentColorImg').style.backgroundColor = state.currentColor;
+                document.getElementById('prevColorImg').style.backgroundColor = state.prevColor;
             }
         }
     }
 
 
+    /*_________________________________________________*/
 
+    //KEYBOARD   m - move; p - picker; t - transform; b - color bucket tool;
+    document.addEventListener('keydown', function (event) {
+        let doEvent = new Event('click');
+        if (event.key === 'p') {
+            document.getElementById('picker').dispatchEvent(doEvent);
+        }
+        if (event.key === 'b') {
+            document.getElementById('bucket').dispatchEvent(doEvent);
+        }
+        if (event.key === 't') {
+            document.getElementById('transform').dispatchEvent(doEvent);
+        }
+        if (event.key === 'm') {
+            document.getElementById('move').dispatchEvent(doEvent);
+        }
+    });
 
+    /*_________________________________________________*/
 
+    //CHOOSE MOVE TOOL
+    ifMove(document.getElementById('move'));
 
+    function ifMove(elem) {
+        elem.onclick = function () {
+            if (state.currentTool === 'move') {
+                state.currentTool = '';
+                localStorage.setItem("myState", JSON.stringify(state));
+                document.getElementById('move').style.backgroundColor = '';
+            }
+            else {
+                //reset colors of others elements
+                document.getElementById('bucket').style.backgroundColor = '';
+                document.getElementById('picker').style.backgroundColor = '';
+                document.getElementById('transform').style.backgroundColor = '';
 
-
-
-
-
-    /*
-
-    //move button
-        dragElement(document.getElementsByClassName('moveImg')[0]);
-
-
-    //transform
-        transformElement(document.getElementsByClassName('transformImg')[0]);
-        function transformElement(elem) {
-            let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-            elem.onmousedown = function (e) {
-                pos1 = 0; pos2 = 0; pos3 = 0; pos4 = 0;
-                elem.style.position = 'relative';
-                e = e || window.event;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmousemove = moveAt;
-                document.onmouseup = function () {
-                    document.onmousemove = null;
-                    elem.style.left = 0;
-                    elem.style.top = 0;
-                    document.onmouseup = null;
-                }
-            };
-
-            function moveAt(e) {
-                e = e || window.event;
-                pos1 += pos3 - e.clientX;
-                pos2 += pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-
-                elem.style.top = - pos2 + 'px';
-                console.log(elem.style.top);
-                elem.style.left =  - pos1 + 'px';
+                state.currentTool = 'move';
+                localStorage.setItem("myState", JSON.stringify(state));
+                document.getElementById('move').style.backgroundColor = 'red';
             }
         }
+    }
 
-    /*
-    //dragElement(document.getElementsByClassName('bucketImg')[0]);
-        //DRAG ELEMENT
-        function dragElement(elem) {
-            let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-            elem.onmousedown = function (e) {
-                pos1 = 0; pos2 = 0; pos3 = 0; pos4 = 0;
-                elem.style.position = 'relative';
-                e = e || window.event;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmousemove = moveAt;
-                document.onmouseup = function () {
-                    document.onmousemove = null;
-                    elem.style.left = 0;
-                    elem.style.top = 0;
-                    document.onmouseup = null;
+    /*_________________________________________________*/
+
+    //CANVAS_ITEM FUNCTIONAL
+    let count = false; //indicators for move tool
+    let orderTemp; //indicators for move tool
+    let elemTemp;
+    Array.from(document.getElementsByClassName('canvas_item')).forEach(elem => {
+        elem.onclick = function itemClick() {
+            let style = getComputedStyle(elem);
+            //bucket listener
+            if (state.currentTool === 'bucket') {
+                elem.style.backgroundColor = state.currentColor;
+            }
+            //transform listener
+            if (state.currentTool === 'transform') {
+                if (elem.style.borderRadius === '') {
+                    elem.style.borderRadius = 'calc((60vh - 20px) / 3)';
                 }
-            };
-            function moveAt(e) {
-                e = e || window.event;
-                pos1 += pos3 - e.clientX;
-                pos2 += pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                elem.style.top = - pos2 + 'px';
-                console.log(elem.style.top);
-                elem.style.left =  - pos1 + 'px';
+                else {
+                    elem.style.borderRadius = '';
+                }
+            }
+            //color picker listener
+            if (state.currentTool === 'picker') {
+                //check for duplicate colors
+                if (style.backgroundColor !== state.currentColor) {
+                    let temp = state.currentColor;
+                    state.currentColor = style.backgroundColor;
+                    state.prevColor = temp;
+                    localStorage.setItem("myState", JSON.stringify(state));
+                    //SET CURRENT AND PREV COLORS IN HTML
+                    document.getElementById('currentColorImg').style.backgroundColor = state.currentColor;
+                    document.getElementById('prevColorImg').style.backgroundColor = state.prevColor;
+                }
+            }
+
+            if (state.currentTool === 'move') {
+                if (count === false) {
+                    orderTemp = style.order; //set order element to move
+                    elemTemp = elem;
+                    count = !count;
+                }
+                else {
+                    elem.style.order = orderTemp;
+                    elemTemp.style.order = style.order;
+                    count = !count;
+                }
             }
         }
-        /*
-        //CHOOSE TOOL WITH ICON CURSOR (not work well)
-        function chooseTool(elem){
-            let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-            elem.onclick = function temp(e) {
-                if(state.currentTool===''){
-                    state.currentTool='bucket';
-                    localStorage.setItem("myState",JSON.stringify(state));
-                    pos1 = 0; pos2 = 0; pos3 = 0; pos4 = 0;
-                    elem.style.position = 'relative';
-                    e = e || window.event;
-                    pos3 = e.clientX;
-                    pos4 = e.clientY;
-                    document.onmousemove = moveAt;
-                }
-                else if(state.currentTool==='bucket'){
-                    console.log(e.target);
-                    state.currentTool='';
-                    document.onmousemove = null;
-                    elem.style.left = 0;
-                    elem.style.top = 0;
-                }
-            };
-
-            function moveAt(e) {
-                e = e || window.event;
-                pos1 += pos3 - e.clientX;
-                pos2 += pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                elem.style.top = - pos2 + 'px';
-                elem.style.left =  - pos1 + 'px';
-            }
-        }
-    */
-
+    });
+    /*_________________________________________________*/
 };
